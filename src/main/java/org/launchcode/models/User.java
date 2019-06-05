47b8b7ee2e475cca3f1ba991d28aa08e0abd7798
farside1969 <farside1969@hotmail.com>
2019-06-05 16:55:52 -0500

    added signup studio part 1

---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,50 @@
+package org.launchcode.models;
+
+public class User {
+
+    private static int nextId = 0;
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    private User() {
+        this.id = nextId++;
+    }
+
+    public User(String username, String email, String password) {
+        this();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
