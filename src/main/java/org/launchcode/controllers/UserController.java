0fa18a4d555462d3ecb273e9c794ede26f583735
farside1969 <farside1969@hotmail.com>
2019-06-07 16:55:34 -0500

    added model attribute User

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -3,9 +3,13 @@ package org.launchcode.controllers;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.jws.soap.SOAPBinding;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
@@ -14,49 +18,31 @@ public class UserController {
     @RequestMapping(value = "add")
     public String addForm(Model model) {
         model.addAttribute("title", "Signup");
+    //added new User attribute
+        model.addAttribute(new User());
 
         return "user/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
+    public String add(Model model, @ModelAttribute @Valid User user, Errors error, String verify) {
+
+        if (error.hasErrors()) {
+            model.addAttribute("user", user);
+
+            return "user/add";
+        }
+
         if (user.getPassword().equals(verify)) {
             model.addAttribute("user", user);
             return "user/index";
         } else {
+    //preserves username and email if passwords don't match
             model.addAttribute("username", user.getUsername());
             model.addAttribute("email", user.getEmail());
+            model.addAttribute("verifyError", "Passwords must match");
 
             return "user/add";
         }
     }
-
-/*    //request path: /user
-    @RequestMapping(value = "")
-    public String index(Model model) {
-
-        return "user/add";
-    }
-
-    @RequestMapping(value="add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, String verify) {
-        if (user.equals(verify));
-
-        return "user/add";
-    }
-*/
-
-/*    @RequestMapping(value = "add", method = RequestMethod.GET)
-    public String displayAddUserForm(Model model) {
-        model.addAttribute("title", "Add User");
-        return "user/add";
-    }
-
-    @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddUserForm(@ModelAttribute User newUser) {
-        CheeseData.add(newCheese);
-        return "redirect:";
-    }*/
-
-
 }
