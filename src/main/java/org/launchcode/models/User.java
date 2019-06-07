0f4c8abdced03c2ce9f39f52f4ce15506eff3c80
farside1969 <farside1969@hotmail.com>
2019-06-07 16:56:19 -0500

    added @NotNull

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,15 +1,28 @@
 package org.launchcode.models;
 
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
 
     private static int nextId = 0;
 
     private int id;
+
+    @NotNull
+    @Size(min=5, max=15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotNull
+    @Size(min=6, message = "Not a Gouda password")
     private String password;
 
-    private User() {
+    public User() {
         this.id = nextId++;
     }
 
